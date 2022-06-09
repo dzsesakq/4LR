ed7e5618acdf8dee9166cf5a7c0ead77137d1d80
DZSESAKQ <jahongirabdikadirov@gmail.com>
2022-06-09 12:45:58 +0300

    2

---
+++ 2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+if __name__ == '__main__':
+    s = input("Enter your word")
+    if "a" in s:
+        print(["a"])
+    else:
+        print("no")
